e54b3031b1cf107c8f3008d0b5034561a5543461
Techmad <techmad60@gmail.com>
2024-10-04 08:17:46 -0700

    first commit

---
+++ fonts/fonts.tsx
@@ -0,0 +1,18 @@
+import { Montserrat, Merriweather} from 'next/font/google';
+ 
+export const montsy = Montserrat({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const merri = Merriweather({
+  weight: ['300', '400', '700', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+
+   
+
+    
