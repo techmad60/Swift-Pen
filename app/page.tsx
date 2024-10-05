15f07a3ed0296f4bacd07c1b620e9b118a6f3e3b
Techmad <techmad60@gmail.com>
2024-10-05 06:12:40 -0700

    Push ooo

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import { montsy } from "@/fonts/fonts";
-import { FaArrowRight } from 'react-icons/fa';
 import Header from "@/components/Header";
 import Main from "@/components/Main";
 import Footer from "@/components/Footer";
